3f165fd6efa686a484424c93c3bc9048d67b1814
Md Abu Al Saeid <khanrajon5@gmail.com>
2023-10-11 00:06:29 +0300

    if and var added

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 function addTask() {
     var task = document.getElementById("task").value;
+    if (task !== "") {
+        var list = document.getElementById("taskList");
+        var li = document.createElement("li");
+        li.innerHTML = task + " <button onclick='deleteTask(this)'>Delete</button>";
+        list.appendChild(li);
+        document.getElementById("task").value = "";
+    }
+}
